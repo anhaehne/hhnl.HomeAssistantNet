69b121e1381ce31672152dcbfbedab5523c8eb7f
Andre Hähnel <andrehaehnel@hotmail.de>
2021-07-07 10:01:46 +0200

    Added better logging for when sensor data is unkown or invalid.

--- c_sharp_for_home_assistant/src/hhnl.HomeAssistantNet/hhnl.HomeAssistantNet.Shared/Entities/Sensor.cs
+++ c_sharp_for_home_assistant/src/hhnl.HomeAssistantNet/hhnl.HomeAssistantNet.Shared/Entities/Sensor.cs
@@ -1,5 +1,6 @@
-﻿using System.Text.Json;
-using hhnl.HomeAssistantNet.Shared.HomeAssistantConnection;
+﻿using hhnl.HomeAssistantNet.Shared.HomeAssistantConnection;
+using System;
+using System.Text.Json;
 
 namespace hhnl.HomeAssistantNet.Shared.Entities
 {
@@ -10,9 +11,38 @@ namespace hhnl.HomeAssistantNet.Shared.Entities
         {
         }
 
+        public bool ValueIsUnknown => State is null || State?.ToString() == "unknown";
+
+        /// <summary>
+        /// Deserializes the state into the given type.
+        /// Returns <c>default</c> if the State is null or the value is 'unknown'.
+        /// </summary>
+        /// <typeparam name="T">The type to deserialize into.</typeparam>
+        /// <returns>The value or <c>default</c></returns>
         public T? GetValue<T>()
         {
-            return State is null ? default : JsonSerializer.Deserialize<T>(State);
+            if (ValueIsUnknown)
+            {
+                return default;
+            }
+
+            try
+            {
+                return JsonSerializer.Deserialize<T>(State!);
+            }
+            catch (JsonException ex)
+            {
+                throw new SensorStateInvalidException($"Unable to convert sensor state '{State?.ToString()}' to type '{typeof(T)}'.", ex);
+            }
+        }
+
+        public class SensorStateInvalidException : Exception
+        {
+            public SensorStateInvalidException(string message, Exception innerException)
+                : base(message, innerException)
+            {
+
+            }
         }
     }
 }
