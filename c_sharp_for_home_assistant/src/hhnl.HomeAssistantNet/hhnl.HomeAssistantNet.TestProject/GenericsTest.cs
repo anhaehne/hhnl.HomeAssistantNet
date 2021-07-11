10e0ed9098d020a0bffd9d1537b041db6dd5087e
Andre Hähnel <andrehaehnel@hotmail.de>
2021-07-11 14:37:55 +0200

    Merged master into dev

--- c_sharp_for_home_assistant/src/hhnl.HomeAssistantNet/hhnl.HomeAssistantNet.TestProject/GenericsTest.cs
+++ c_sharp_for_home_assistant/src/hhnl.HomeAssistantNet/hhnl.HomeAssistantNet.TestProject/GenericsTest.cs
@@ -1,10 +1,7 @@
-﻿using hhnl.HomeAssistantNet.Shared.Automation;
+﻿using hhnl.HomeAssistantNet.Automations.Automation;
+using hhnl.HomeAssistantNet.Shared.Automation;
 using hhnl.HomeAssistantNet.Shared.Entities;
 using HomeAssistant;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace hhnl.HomeAssistantNet.TestProject
@@ -12,7 +9,7 @@ namespace hhnl.HomeAssistantNet.TestProject
     public abstract class ToggleLightAutomations<T> where T : Light
     {
         [Automation]
-        public async Task ToggleLight([NoTrack]T @switch)
+        public async Task ToggleLight([NoTrack] T @switch)
         {
             await @switch.ToggleAsync();
         }
